807879edeeb88391809b0a4394d38f3d272a6132
Derek <dpiersonrmrs@gmail.com>
2016-05-07 22:02:06 -0600

    Update basins_via_DEM.py

--- Wildfire-Basins/basins_via_DEM.py
+++ Wildfire-Basins/basins_via_DEM.py
@@ -5,23 +5,44 @@
 
 #NOTES: 
 
+
 #Startup code for running script in QGIS console
+import os
+from PyQt4.QtCore import * 
 
-Paste here
+settings = QSettings()
+settings.setValue("pythonConsole/lastDirPath", QDir.homePath())
+
+from qgis.core import *
+
+# supply path to where is your qgis installed
+QgsApplication.setPrefixPath("E:/Program Files/QGIS Essen/bin", True)
+print "Current working dir : %s" % os.getcwd()
+
+# load providers
+QgsApplication.initQgis()
 
 
+#####################
+#Start of QGIS Work
+#####################
 
-#Parameter list
-  #Set work path
-  #Fire name
-  #DEM path
-  #Wildfire shape path
-  #Wildfire burn severity path
-  #Stream network size
-  #Fire buffer size
+#Variable list - Input parameters for each individual wildfire analysis 
+FIRE_NAME = "Hayman"
+DEM = "P:/QGIS/WildfireChem Project/Hayman_py/DEM/USGS_NED_1_n40w106_IMG.img"
+FIRE_SHAPE = "P:/QGIS/WildfireChem Project/Hayman_py/Shapes/Hayman_shape.shp" 
+#SEVRT =
+Stream_size = 10000
+Fire_buffer = 10000
 
 
-#Load DEM
+#Load full size DEM - Download 1/3 arc second from USGS
+fileInfo = QFileInfo(DEM)
+baseName = fileInfo.baseName()
+rlayer = QgsRasterLayer(DEM, baseName)
+if not rlayer.isValid():
+  print "Layer failed to load!"
+
   #Project to UTM for USA
 
 

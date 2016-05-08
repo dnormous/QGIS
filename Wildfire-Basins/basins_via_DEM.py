728273f9bcacd4e67bf7f8be868570e3d2274b4c
Derek <dpiersonrmrs@gmail.com>
2016-05-08 13:00:12 -0600

    Update basins_via_DEM.py

--- Wildfire-Basins/basins_via_DEM.py
+++ Wildfire-Basins/basins_via_DEM.py
@@ -11,6 +11,9 @@ import os
 from PyQt4.QtCore import * 
 from qgis.core import *
 from qgis.analysis import *
+import processing
+
+
 
 settings = QSettings()
 settings.setValue("pythonConsole/lastDirPath", QDir.homePath())
@@ -29,8 +32,9 @@ QgsApplication.initQgis()
 
 #Variable list - Input parameters for each individual wildfire analysis 
 FIRE_NAME = "Hayman"
-DEM = "P:/QGIS/WildfireChem Project/Hayman_py/DEM/Hayman_UTM_DEM.tif"
-FIRE_SHAPE = "P:/QGIS/WildfireChem Project/Hayman_py/Shapes/Hayman_UTM_shape.shp" 
+PATH = "P:/QGIS/WildfireChem Project/Hayman_py/"
+DEM = "DEM/Hayman_UTM_DEM.tif"
+FIRE_SHAPE = "Shapes/Hayman_UTM_shape.shp" 
 #SEVRT =
 Stream_size = 10000
 Fire_buffer = 10000
@@ -51,9 +55,9 @@ print "Map units:", mycrs.mapUnits() # meters = 0, degrees = 2
 
 
 #Load full size DEM - Download 1/3 arc second from USGS
-fileInfo = QFileInfo(DEM)
+fileInfo = QFileInfo(PATH+DEM)
 baseName = fileInfo.baseName()
-rlayer = QgsRasterLayer(DEM, baseName)
+rlayer = QgsRasterLayer(PATH+DEM, baseName)
 
 if rlayer.isValid():
   print "DEM loaded successfully!"
@@ -63,7 +67,7 @@ else:
 #??? How to project raster from NAD83 to UTM Zone 13N
 
 #Add rastor layer to interface
-ifaceraslayer = iface.addRasterLayer(DEM, FIRE_NAME + "_DEM")
+ifaceraslayer = iface.addRasterLayer(PATH+DEM, FIRE_NAME + "_DEM")
 if ifaceraslayer.isValid():
   print "Raster added to canvas successfully!"
 else:
@@ -72,7 +76,7 @@ else:
 
 #Load wildfire shapefile
 #Import vector layer from file
-fire_layer = QgsVectorLayer(FIRE_SHAPE, FIRE_NAME + "_Shape", "ogr")
+fire_layer = QgsVectorLayer(PATH+FIRE_SHAPE, FIRE_NAME + "_Shape", "ogr")
 if fire_layer.isValid():
   print "Fire shape layer loaded successfully!"
 else:
@@ -82,7 +86,7 @@ else:
 #fire_layer.setCrs(mycrs,True)
 
 #Load vector layer to interface
-fire_ifacelayer = iface.addVectorLayer(FIRE_SHAPE, FIRE_NAME + "_Shape", "ogr")
+fire_ifacelayer = iface.addVectorLayer(PATH+FIRE_SHAPE, FIRE_NAME + "_Shape", "ogr")
 if fire_ifacelayer.isValid():
   print "Fire layer added to canvas successfully!"
 else:
@@ -90,32 +94,33 @@ else:
 
 
 #Create buffer around fire shape
-QgsGeometryAnalyzer().buffer(fire_layer, "P:/QGIS/WildfireChem Project/Hayman_py/Shapes/Buffer.shp", 50, False, False, -1)
+QgsGeometryAnalyzer().buffer(fire_layer, PATH + "Shapes/" + FIRE_NAME + "_Buffer.shp", Fire_buffer, False, False, -1)
 
+#Load newly created fire buffer vector
+fire_buffer = QgsVectorLayer(PATH + "Shapes/" + FIRE_NAME + "_Buffer.shp", FIRE_NAME + "_Buffer", "ogr")
+if fire_buffer.isValid():
+  print "Fire buffer layer loaded successfully!"
+else:
+  print "Fire buffer failed to load!" 
+  
+#Load fire buffer vector layer to interface
+buffer_ifacelayer = iface.addVectorLayer(PATH + "Shapes/" + FIRE_NAME + "_Buffer.shp", FIRE_NAME + "_Buffer", "ogr")
+if fire_ifacelayer.isValid():
+  print "Fire buffer layer added to canvas successfully!"
+else:
+    print "Fire buffer layer failed to load to canvas!"
+    
 
-
-
-
-
-
-
-
-#Load wildfire burn severity layer
-
-#Clip DEM by buffer
+#Clip DEM by fire buffer
+#processing.alglist("clip")   #<-- Finds processing algorithms related to "clip"
+processing.runalg("gdalogr:cliprasterbymasklayer","P:/QGIS/WildfireChem Project/Hayman_py/DEM/Hayman_UTM_DEM.tif","P:/QGIS/WildfireChem Project/Hayman_py/Shapes/Hayman_Buffer.shp","-9999",False,False,False,5,4,75,6,1,False,0,False,"","P:/QGIS/WildfireChem Project/DEMs/Hayman DEM/cliptest.tif")
 
 #Run r.watershed on clipped DEM
-  #Save files
+
+  
+  
   #Convert stream layer to vector
   #Convert basin layer to vector
-  
-#Union basins and burn severity
-  #Add layer to map with style
-
-#Data table analysis...
-  
-
-
-
-  
+    
+    
 
